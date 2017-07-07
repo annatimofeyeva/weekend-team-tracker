319f9553fd36f65c795baf6150d125aacc35ea1f
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-07-07 14:11:03 -0700

    add user session to App.java and branching to index.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,11 +8,28 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 //create a route that render homepage (add a Team, view all teams)
-  public ("/"), (request, response) -> {
-    Map<String, Object> model = new HashMap,String, Object>();
-    model.put("template," "templates/index.vtl");
-    return new ModelAndView(model, layout);
-  }
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("team", request.session().attribute("team"));
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+//create a post route after user click submit button
+    post("/teams", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      String description = request.queryParams("description");
+      Team newTeam = new Team(description);
+      request.session().attribute("team", newTeam);
+
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  /*on order to display our team in index template - the roort route
+  must retrive it from the user session. Then it will need to provide it
+  to the index template - so we need to modify our get ("/")route
+  and after this we also need to modify index.vtl*/
+
 
   }
 }
