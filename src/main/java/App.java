be50ae7fc9017ac25db5f6ced752379ea78a7fe8
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-07-07 15:06:13 -0700

    modified index.vtl and add sucess.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,7 @@
 import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -6,23 +9,30 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
-    String layout = "templates/layout.vtl";
+    String layout = "template/layout.vtl";
 //create a route that render homepage (add a Team, view all teams)
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("team", request.session().attribute("team"));
-      model.put("template", "templates/index.vtl");
+      model.put("teams", request.session().attribute("teams"));
+      model.put("template", "template/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 //create a post route after user click submit button
     post("/teams", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+/*create the arrayList for teams - to create ans display
+multiple teams in the session and display them all in the template*/
+      ArrayList<Team> teams = request.session().attribute("teams");
+      if (teams == null) {
+        teams = new ArrayList<Team>();
+        request.session().attribute("teams", teams);
+      }
 
       String description = request.queryParams("description");
       Team newTeam = new Team(description);
-      request.session().attribute("team", newTeam);
+      teams.add(newTeam);
 
-      model.put("template", "templates/success.vtl");
+      model.put("template", "template/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   /*on order to display our team in index template - the roort route
